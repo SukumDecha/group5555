a97caf82ad8d53edbe809a091bdd4186491c5861
ludacapo4646 <bp396043@gmail.com>
2025-02-05 21:00:05 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import { LanguageProvider } from "./LanguageContext";
-// import Navbar from "./Navbar";
-// import Home from "./Home";
-// import Booking from "./Booking";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LanguageProvider } from "./LanguageContext";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Booking from "./Booking";
 
-// function App() {
-//   return (
-//     <LanguageProvider>
-//       <Router>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/booking" element={<Booking />} />
-//         </Routes>
-//       </Router>
-//     </LanguageProvider>
-//   );
-// }
+function App() {
+  return (
+    <LanguageProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/booking" element={<Booking />} />
+        </Routes>
+      </Router>
+    </LanguageProvider>
+  );
+}
 
-// export default App;
+export default App;
